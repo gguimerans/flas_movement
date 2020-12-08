69d3ef5d459588459d3613bed376dc6097f28443
Gonzalo <gonzaloguimerans@gmail.com>
2020-12-08 16:28:46 +0100

    actualizacion nuevo VSC

---
+++ movements/views.py
@@ -0,0 +1,20 @@
+from movements import app
+from flask import render_template 
+import csv
+
+@app.route("/")
+def listaMovimientos():
+    fIngresos = open("movements/data/basededatos.csv", "r")
+    csvReader = csv.reader(fIngresos, delimiter=',', quotechar='"')
+    ingresos = list(csvReader)
+    sumador = 0
+    for ingreso in ingresos:
+        sumador+=float(ingreso[2])
+
+    print(ingresos)    
+
+    return render_template("movementsList.html",datos=ingresos, total=sumador)
+
+    @app.route("/creaalta")
+    def nuevoIngreso():
+        return "Ya lo veremos"
